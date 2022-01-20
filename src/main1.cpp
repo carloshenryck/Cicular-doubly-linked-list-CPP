2659d1b85bbffe0faf1e379f9def3cdfb3b4c041
aSTRonuun <vitormanoel00710@gmail.com>
2022-01-20 00:54:29 -0300

    Funcao swap adicionada

--- src/main1.cpp
+++ src/main1.cpp
@@ -7,19 +7,22 @@ int main() {
 
     List l1;
 
-    l1.push_back(1);
-
-    l1.removeAll(1);
-
-    l1.push_back(1);
     l1.push_back(2);
+    l1.push_back(4);
+    l1.push_back(6);
 
-    l1.removeAll(2);
+    List l2;
 
-    l1.push_back(2);
+    l2.push_back(1);
+
+    l1.swap(l2);
+
+    l2.push_back(8);
+    l2.push_back(10);
+
     l1.push_back(3);
+    l1.push_back(5);
 
-    l1.removeAll(2);
 
 
     return 0;
